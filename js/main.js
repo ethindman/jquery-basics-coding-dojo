5b85b7cc257f2ab3d63c64a4189297595ba54b4d
Elliott Hindman <portokaliajapan@gmail.com>
2015-02-23 10:22:18 -0800

    Changes functions.js to main.js

--- js/main.js
+++ js/main.js
@@ -13,19 +13,19 @@ $(document).ready(function(){
 	$('#show').click(function() {
 		$(this).closest('div').find('.hidden').show();
 		$('#show').hide();
-	})
+	});
 
 	//Hide content currently displayed
 	$('#hide').click(function() {
 		$(this).closest('div').hide();
 		$('#show').show();
-	})
+	});
 
 	//Changes background color on click
 	$('#change').click(function() {
 		var bgcolorchange = new Array("#ff5051", "#70d16f", "#6cd4d1", "#6bc0c9", "#5bc1a6","#3aba9a","#f26e53", "#50c0e9","#3aaed9","#ffce55","#f4ba41", "#5386c5");
 		document.body.style.background = bgcolorchange[Math.floor(Math.random()*bgcolorchange.length)];
-	})
+	});
 
 	//Get value of an item
 	$('#get-input').click(function() {
@@ -36,13 +36,13 @@ $(document).ready(function(){
 		} else {
 			alert("Oops! You didn't type anything!");
 			}
-	})
+	});
 
 	//Clear the output field
 	$('.clear').click(function() {
 	var $this = $(this).closest('div');
 		$this.find('.output').empty();
-	})
+	});
 
 	//Change background color on mouseenter $ mouseleave
   $("#graffitti").on("mouseenter", function() {
@@ -56,7 +56,7 @@ $(document).ready(function(){
   $('.toggle').click(function() {
   	$(this).closest('div').find('img').toggle();
   	// scrollByLines(100);
-  })
+  });
 
   //Does a fadeout on a list item 
   var count = 0;
@@ -66,7 +66,7 @@ $(document).ready(function(){
   	if (count >= 5) {
   		$('.success').delay(300).fadeIn(800);
   	}
-  })
+  });
 
   //Hover over image and change text below
 	// $('#graffitti').hover(function() {
